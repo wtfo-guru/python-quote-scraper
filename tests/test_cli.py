3a1b86bcabf037f3d4606e81975e820e3f4a702e
Quien Sabe <qs5579@mail.com>
2025-02-11 11:55:35 -0400

    test: coverage at 64%

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -1,8 +1,27 @@
 import pytest
+import testfixtures
 from click.testing import CliRunner
 
 from quote_scraper import cli
 from quote_scraper.constants import VERSION
+
+HELP = """Usage: main [OPTIONS]
+
+  Tool to scrape quotes from web pages.
+
+Options:
+  -a, --author TEXT             Specify author of quotes.
+  -b, --brainy                  Specify scrape brainyquotes of the day.
+  --cache TEXT                  Specify cache directory (default ~/.cache/quote-
+                                scraper)
+  -c, --category TEXT           Specify category of quotes.
+  -d, --debug / --no-debug      Specify debug mode, default: False
+  -i, --inspiring               Specify scrape inquiring quote of the day.
+  -u, --url TEXT                Specify scrape url.
+  -v, --verbose / --no-verbose  Specify verbose mode, default: False
+  --version                     Show the version and exit.
+  --help                        Show this message and exit.
+"""
 
 
 @pytest.fixture
@@ -19,17 +38,9 @@ def test_cli_version(runner):
     assert test_result.output.strip() == "main, version {0}".format(VERSION)
 
 
-# def test_cli_with_option(runner):
-#     """Function test_cli_with_option."""
-#     resultado = runner.invoke(cli.main, ["--as-cowboy"])
-#     assert not resultado.exception
-#     assert resultado.exit_code == 0
-#     assert resultado.output.strip() == "Howdy, world."
-
-
-# def test_cli_with_arg(runner):
-#     """Function test_cli_with_arg."""
-#     resultado = runner.invoke(cli.main, ["Quien"])
-#     assert resultado.exit_code == 0
-#     assert not resultado.exception
-#     assert resultado.output.strip() == "Hello, Quien."
+def test_cli_help(runner):
+    """Function test_cli_version."""
+    test_result = runner.invoke(cli.main, ["--help"])
+    assert not test_result.exception
+    assert test_result.exit_code == 0
+    testfixtures.compare(HELP, test_result.output)
